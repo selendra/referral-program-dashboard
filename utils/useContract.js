806e2031f38afac6c74d5d57bc5143ea9b67f2f6
combo <combo@combos-MacBook-Pro.local>
2021-06-24 09:29:31 +0700

    metamask

--- utils/useContract.js
+++ utils/useContract.js
@@ -4,7 +4,7 @@ import { useWeb3 } from './useWeb3';
 
 
 export function useContract() {
-  const contractAddress = '0xd84D89d5C9Df06755b5D591794241d3FD20669Ce';
+  const contractAddress = '0xc7fe9232c55fab39fd7c89aa459aa03c55ee283c';
 
   const web3 = useWeb3();
   let contract = new web3.eth.Contract(abi, contractAddress);
